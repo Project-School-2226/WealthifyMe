e31440cf2eab449e1f0cf5bd2e72c764dc54c83d
Shaik Amaan <shaikamaanahmed2585@gmail.com>
2024-11-22 15:07:12 +0530

    added init of categories for new login

--- server/routes/save_user_details.js
+++ server/routes/save_user_details.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
-
+const { User, initialiseDefaultCategoriesForUser } = require('../models/users');
 
 router.post('/save', async (req, res) => {
-    const { user_id,email, username } = req.body;
+    const { user_id, email, username } = req.body;
 
-    if(!user_id || !email || !username) {
-        console.log("Please provide all details");
-        return res.status(400).json({ message: "Please provide all details" });
+    if (!user_id || !email || !username) {
+        console.log("Please fill in all fields");
+        return res.status(400).json({ message: "Please fill in all fields" });
     }
 
-    const user = new User({ user_id,email, username});
-
     try {
+        const existingUser = await User.findOne({ user_id });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = new User({ user_id, email, username });
         const newUser = await user.save();
+
         if (newUser) {
+            await initialiseDefaultCategoriesForUser(user_id);
             res.status(200).json({ message: "User saved" });
         } else {
             res.status(400).json({ message: "User not saved" });
         }
     } catch (error) {
+        console.log(error.message);
         res.status(500).json({ message: error.message });
     }
 });
